9b8ebef7419e820446a2dea24b79351ef6d6aa9b
Danny Garcia <dgarcia1724@berkeley.edu>
2023-09-24 01:41:48 -0700

    counter done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleAdd() {
+    setCount((count) => count + 1);
+  }
+
   return (
     <div>
-      <h1>lol</h1>
+      <h1>Counter</h1>
+      <h1>{count}</h1>
+      <button onClick={handleAdd}>+</button>
     </div>
   );
 }
